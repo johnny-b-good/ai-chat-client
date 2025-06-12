d99c3bb493cce59d96923b71f93823a6e1ed4c43
Anton Melnikov <melnikov.anton@gmail.com>
2025-06-12 16:40:58 +0300

    Show characters in the chat window

--- src/app/chats/[id]/ui/ChatHeader.tsx
+++ src/app/chats/[id]/ui/ChatHeader.tsx
@@ -1,20 +1,38 @@
 import { type FC } from "react";
 
-import { Avatar, AvatarFallback } from "@/components/ui";
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui";
 import { Header, BackButton } from "@/app/ui";
+import { type Character, type Model } from "@/generated/prisma";
 
 export type ChatHeaderProps = {
-  modelName: string;
+  model: Model;
+  character: Character | null;
 };
 
-export const ChatHeader: FC<ChatHeaderProps> = ({ modelName }) => {
+export const ChatHeader: FC<ChatHeaderProps> = ({ model, character }) => {
   return (
     <Header left={<BackButton href="/chats" />}>
       <div className="flex items-center gap-2">
         <Avatar>
+          {character && (
+            <AvatarImage
+              src={`data:image/png;base64,${character.avatarBase64}`}
+              alt={character.name}
+            />
+          )}
           <AvatarFallback>AI</AvatarFallback>
         </Avatar>
-        <div className="text-lg">{modelName}</div>
+
+        <div className="flex items-baseline gap-2">
+          {character ? (
+            <>
+              <div className="text-lg">{character.name}</div>
+              <div className="text-xs text-slate-500">by {model.name}</div>
+            </>
+          ) : (
+            <div className="text-lg">{model.name}</div>
+          )}
+        </div>
       </div>
     </Header>
   );
