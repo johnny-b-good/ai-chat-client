f174678cc04888a14c7b3b6f91965ee35e587212
Anton Melnikov <melnikov.anton@gmail.com>
2025-06-18 13:01:09 +0300

    Better chat log representation for summary creation

--- src/app/chats/lib/actions.ts
+++ src/app/chats/lib/actions.ts
@@ -6,7 +6,9 @@ import ollama from "ollama";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { generateObject } from "ai";
+
 import { openai } from "@/app/lib/openai";
+import { messagePartsSchema } from "@/app/lib/schemas";
 
 export type NewChatFormState = {
   errors?: {
@@ -95,13 +97,39 @@ export const deleteChat = async (id: number) => {
 };
 
 export const summarize = async (id: number) => {
+  const chat = await prisma.chat.findFirstOrThrow({
+    where: { id },
+    include: { character: true, model: true },
+  });
   const messages = await prisma.message.findMany({ where: { chatId: id } });
 
-  const messageString = JSON.stringify(messages, undefined, 4);
+  const chatLog = messages
+    .map((message) => {
+      const messageText = messagePartsSchema
+        .parse(message.parts)
+        .map((part) => part.text)
+        .join("\n");
+
+      if (message.role === "user" || message.role === "assistant") {
+        const author =
+          message.role === "user"
+            ? "User"
+            : chat.character
+              ? chat.character.name
+              : `Model "${chat.model.name}"`;
+
+        return `${author} said at ${message.createdAt.toISOString()}:\n${messageText}`;
+      } else {
+        return null;
+      }
+    })
+    .filter((text) => text !== null)
+    .join("\n");
 
   if (!process.env.SUMMARIZE_MODEL) {
     throw new Error("Env variable SUMMARIZE_MODEL is not set");
   }
+
   const {
     object: { name, summary },
   } = await generateObject({
@@ -120,7 +148,7 @@ export const summarize = async (id: number) => {
           "A short summary describing topics, events, reactions that occured this chat. Summary should be a short paragraph, containing maximum of five sentences.",
         ),
     }),
-    prompt: `Generate a JSON object strictly following supplied schema for the following dialog:\n${messageString}`,
+    prompt: `Generate a JSON object strictly following supplied schema for the following dialog:\n${chatLog}`,
   });
 
   await prisma.chat.update({
