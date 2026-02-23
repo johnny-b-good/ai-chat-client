8c7b86f378c99291b54ebe761de052afdd68943e
Anton Melnikov <melnikov.anton@gmail.com>
2026-02-23 11:41:43 +0300

    Removed unused packages

--- src/app/chats/[id]/ui/MessageForm.tsx
+++ src/app/chats/[id]/ui/MessageForm.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { type FC, type KeyboardEvent, useRef } from "react";
-import { PaperAirplaneIcon } from "@heroicons/react/24/outline";
+import { SendIcon } from "lucide-react";
 
 import { Button, Textarea } from "@/components/ui";
 
@@ -51,7 +51,7 @@ export const MessageForm: FC<MessageFormProps> = ({
         className="absolute top-1/2 right-4 -translate-y-1/2 transform"
         disabled={disabled}
       >
-        <PaperAirplaneIcon className="text-primary size-6" />
+        <SendIcon className="text-primary size-6" />
       </Button>
     </form>
   );
